adcf40c2a6e152556c4d54e4bf69ecaf46387836
Jose Antonio Martinez Vidaurre <jbmw16@gmail.com>
2023-08-29 22:18:10 -0400

    added a basic userspace serial port driver for sending the stuff over to the stm. So far it is hard coded to grab from /sys/hwmon3/temp1_input. Will change this later. Ideally we want to get a hwmon interface, so linux driver will have to be implemented.

--- Src/main.c
+++ Src/main.c
@@ -10,12 +10,15 @@
 #include "stm32f0xx_hal_msp.c"
 #include "stm32f0xx_hal_rcc.h"
 #include "stm32f0xx_hal_uart.h"
+#include "stm32f0xx_hal_usart.h"
 #include "stm32f0xx_nucleo_32.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define COUNTOF(__BUFFER__)   (sizeof(__BUFFER__) / sizeof(*(__BUFFER__)))
 #define FAN_COUNT 1
 #define TXSTARTMESSAGESIZE               (COUNTOF(aTxStartMessage) - 1)
+#define CHUNK_SIZE 2
 
 void initUart(UART_HandleTypeDef* uart);
 void uartPins();
@@ -88,24 +91,6 @@ size_t unCobs (void* stuff, uint8_t* buffer, size_t s) {
   return (writePos - buffer);
 };
 
-
-
-// cobs encode data and send through serial  
-void sendData(void* data, size_t length, UART_HandleTypeDef* art) {
-  int res;
-  res = cobs(data, bufferCOBS, length);
-  //if (HAL_UART_Transmit(art, bufferCOBS, res, 100) != HAL_OK)
-  //  fuck(5000);
-  HAL_UART_Transmit(art, bufferCOBS, res, 100);
-}
-
-
-int parseDriverData(void* data, struct driverData* xd) {
-  xd = (struct driverData*)data;
-  return 0;
-}
-
-
 void getPacket(UART_HandleTypeDef* art, uint8_t* buffer) {
   uint8_t byte[10] = {0};
   do {
@@ -141,6 +126,22 @@ void shit(uint8_t* err) {
     sendData(errorMessage,sizeof(errorMessage), &art);
   }
 }
+
+
+// send in chunks of max 16 bytes
+void printLn(UART_HandleTypeDef* uart, char* message){
+  int length = strlen(message);
+  char* p = message;
+  for (size_t c = 0; c < length;){
+    if ( (length - c) >= CHUNK_SIZE ) {
+      HAL_UART_Transmit(uart, (message + c) , CHUNK_SIZE, 100);
+      c += CHUNK_SIZE;
+    } else {
+      HAL_UART_Transmit(uart, (message + c) , length - c, 100);
+      c += length - c;
+    }
+  }
+};
   
 
 void initUart(UART_HandleTypeDef* uart) {
@@ -172,7 +173,7 @@ void configurePins() {
     GPIO_InitTypeDef sus = {0};
     sus.Pin = GPIO_PIN_8;
     sus.Mode = GPIO_MODE_AF_PP;
-    sus.Speed = GPIO_SPEED_FREQ_LOW;
+    sus.Speed = GPIO_SPEED_MEDIUM;
     sus.Alternate = GPIO_AF2_TIM1;
     sus.Pull = GPIO_PULLUP;
     HAL_GPIO_Init(GPIOA, &sus);
@@ -200,7 +201,7 @@ if (HAL_TIM_PWM_Init(tim) != HAL_OK)
 
 TIM_OC_InitTypeDef oc = {0};
 oc.OCMode = TIM_OCMODE_PWM1;
-oc.Pulse = 1;
+oc.Pulse = 244;
 oc.OCPolarity = TIM_OCPOLARITY_HIGH;
 oc.OCFastMode = TIM_OCFAST_ENABLE;
 
@@ -242,7 +243,7 @@ void SystemClock_Config(void)
 }
 
 int main () {
-  int32_t CH1_DC;
+  int32_t CH1_DC = 0;
   uint8_t xbuffer[64];
   char str[4];
   HAL_Init();
@@ -258,16 +259,28 @@ int main () {
   initUart(&art);
   __HAL_UART_ENABLE(&art);
 
-  //uint8_t error = 1;
-  //if (error != HAL_OK)
-  //  shit(&error);
-  getPacket(&art, xbuffer);
-  // sendData("amo   gus", sizeof("amogus"), &art);
+  while(1) { 
 
+
+
+   
   HAL_TIM_PWM_Start(&tim, TIM_CHANNEL_1);
-  while (1)
-  {
-    HAL_Delay(100);
-  }
+//  while(1) {
+//       while(CH1_DC < 255)
+//        {
+//            TIM1->CCR1 = CH1_DC;
+//            CH1_DC += 1;
+//            HAL_Delay(100);
+//        }
+//        while(CH1_DC > 0)
+//        {
+//            TIM1->CCR1 = CH1_DC;
+//            CH1_DC -= 1;
+//            HAL_Delay(100);
+//        }
+//    }
+
+
+
 };
 
--- flash/flash.sh
+++ flash/flash.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 openocd -f openocd.cfg -c "program ../Src/sus.bin verify reset exit"
